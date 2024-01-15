fe6fa11487eaba533d5699af89d96f4dead98fb1
xianjianlf2 <mark-xian@foxmail.com>
2024-01-15 14:57:24 +0800

    feat: add fiber

--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -4,47 +4,122 @@ function createElement(type, props, ...children) {
     props: {
       ...props,
       children: children.map((child) => {
-        return typeof child === "string" ? createTextNode(child) : child;
-      }),
-    },
-  };
+        return typeof child === 'string' ? createTextNode(child) : child
+      })
+    }
+  }
 }
 
 function createTextNode(text) {
   return {
-    type: "TEXT_ELEMENT",
+    type: 'TEXT_ELEMENT',
     props: {
       nodeValue: text,
-      children: [],
-    },
-  };
+      children: []
+    }
+  }
 }
 
 function render(el, container) {
-  console.log(el);
-  const dom =
-    el.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(el.type);
-
-  // props
-  Object.keys(el.props).forEach((key) => {
-    if (key !== "children") {
-      dom[key] = el.props[key];
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [el]
     }
-  });
-
-  const children = el.props.children;
-  children.forEach((child) => {
-    render(child, dom);
-  });
-
-  container.append(dom);
+  }
+  root = nextUnitOfWork
 }
 
 const React = {
   render,
-  createElement,
-};
+  createElement
+}
+let root = null
+let nextUnitOfWork = null
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  // purpose: render dom at once
+  if (!nextUnitOfWork && root) {
+    commitRoot()
+  }
+  requestIdleCallback(workLoop)
+}
 
-export default React;
+
+function commitRoot() {
+  commitWork(root.child)
+  root = null
+}
+
+function commitWork(fiber) {
+  if (!fiber) return
+  fiber.parent.dom.append(fiber.dom)
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
+    if (key !== 'children') {
+      dom[key] = props[key]
+    }
+  })
+}
+
+function createDom(type) {
+  return type === 'TEXT_ELEMENT'
+    ? document.createTextNode('')
+    : document.createElement(type)
+}
+
+// 1. create a fiber object
+// 2. create child fiber object
+// 3. return next work unit
+function initChildren(fiber) {
+  const children = fiber.props.children
+  let prevChild = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      parent: fiber,
+      dom: null,
+      sibling: null
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+    prevChild = newFiber
+  })
+}
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    // 1. create dom
+    const dom = (fiber.dom = createDom(fiber.type))
+
+    // fiber.parent.dom.append(dom)
+    // 2. handle props
+    updateProps(dom, fiber.props)
+  }
+  // 3. set pointer
+  initChildren(fiber)
+  // 4. return next unit of work
+
+  if (fiber.child) {
+    return fiber.child
+  }
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+  return
+}
+
+requestIdleCallback(workLoop)
+
+export default React
